6048bd1156619e102fa4ab3fd4544a585972ce5e
Ramon // Gravity <r.swilem@gravity.nl>
2022-12-05 10:44:36 +0100

    fix: Update days

--- day4/index.js
+++ day4/index.js
@@ -1,9 +1,61 @@
 module.exports = {
   part1(input) {
     const lines = input.split('\n');
+
+    let contains = 0;
+    for (const line of lines) {
+      const sections = line
+        .split(',')
+        .map((section) => {
+          const [start, end] = section.split('-');
+          return [Number(start), Number(end)];
+        })
+        .sort((a, b) => {
+          return a[0] > b[0] ? 1 : -1;
+        });
+
+      if (sections.length > 2) {
+        console.log('Longer section found!');
+        continue;
+      }
+
+      const [aStart, aEnd] = sections[0];
+      const [bStart, bEnd] = sections[1];
+      if ((aStart <= bStart && aEnd >= bEnd) || (aStart >= bStart && aEnd <= bEnd)) {
+        contains++;
+      }
+    }
+
+    return `${contains} of ${lines.length}`;
   },
 
   part2(input, part1Solution) {
-    //const lines = input.split('\n');
+    const lines = input.split('\n');
+
+    let partial = 0;
+    for (const line of lines) {
+      const sections = line
+        .split(',')
+        .map((section) => {
+          const [start, end] = section.split('-');
+          return [Number(start), Number(end)];
+        })
+        .sort((a, b) => {
+          return a[0] > b[0] ? 1 : -1;
+        });
+
+      if (sections.length > 2) {
+        console.log('Longer section found!');
+        continue;
+      }
+
+      const [aStart, aEnd] = sections[0];
+      const [bStart, bEnd] = sections[1];
+      if (aEnd >= bStart || bEnd <= aStart) {
+        partial++;
+      }
+    }
+
+    return `${partial} of ${lines.length}`;
   }
 };
--- day5/index.js
+++ day5/index.js
@@ -2,60 +2,132 @@ module.exports = {
   part1(input) {
     const lines = input.split('\n');
 
-    let contains = 0;
-    for (const line of lines) {
-      const sections = line
-        .split(',')
-        .map((section) => {
-          const [start, end] = section.split('-');
-          return [Number(start), Number(end)];
-        })
-        .sort((a, b) => {
-          return a[0] > b[0] ? 1 : -1;
-        });
-
-      if (sections.length > 2) {
-        console.log('Longer section found!');
-        continue;
+    // Fill initial arrays
+    const stacks = [];
+    for (const rawLine of lines) {
+      if (!rawLine.includes('[') || rawLine.trim().startsWith('move')) {
+        break;
       }
 
-      const [aStart, aEnd] = sections[0];
-      const [bStart, bEnd] = sections[1];
-      if ((aStart <= bStart && aEnd >= bEnd) || (aStart >= bStart && aEnd <= bEnd)) {
-        contains++;
+      const line = rawLine
+        .replace(/\s\s\s\s\s/gi, ' [ ] ')
+        .replace(/\s\s\s/gi, '[ ]')
+        .replace(/\[\s\]/gi, '[!]')
+        .replace(/\]\[/gi, '] [')
+        .replace(/\s\s/gi, ' ');
+
+      const items = line
+        .split(' ')
+        .filter((item) => item.length > 0)
+        .map((item) => {
+          return item.replace('[', '').replace(']', '');
+        });
+
+      for (let i = 0; i < items.length; i++) {
+        if (!stacks[i]) {
+          stacks[i] = [];
+        }
+
+        if (items[i] === '!') {
+          continue;
+        }
+
+        stacks[i].unshift(items[i]);
       }
     }
 
-    return `${contains} of ${lines.length}`;
+    for (const line of lines) {
+      if (!line.trim().startsWith('move')) {
+        continue;
+      }
+
+      const match = line.match(/move ([0-9]+)? from ([0-9]+)? to ([0-9]+)?/i);
+      const amount = Number(match[1]);
+      const sourceStackIndex = Number(match[2]) - 1;
+      const targetStackIndex = Number(match[3]) - 1;
+
+      if (amount <= 0 || amount > stacks[sourceStackIndex].length) {
+        console.log(`Invalid command! ${amount}: ${line}`);
+        continue;
+      }
+
+      const itemsToMove = stacks[sourceStackIndex].splice(stacks[sourceStackIndex].length - amount, amount).reverse();
+      for (const item of itemsToMove) {
+        stacks[targetStackIndex].push(item);
+      }
+    }
+
+    return this.topStacks(stacks);
   },
 
   part2(input, part1Solution) {
     const lines = input.split('\n');
 
-    let partial = 0;
-    for (const line of lines) {
-      const sections = line
-        .split(',')
-        .map((section) => {
-          const [start, end] = section.split('-');
-          return [Number(start), Number(end)];
-        })
-        .sort((a, b) => {
-          return a[0] > b[0] ? 1 : -1;
-        });
-
-      if (sections.length > 2) {
-        console.log('Longer section found!');
-        continue;
+    // Fill initial arrays
+    const stacks = [];
+    for (const rawLine of lines) {
+      if (!rawLine.includes('[') || rawLine.trim().startsWith('move')) {
+        break;
       }
 
-      const [aStart, aEnd] = sections[0];
-      const [bStart, bEnd] = sections[1];
-      if (aEnd >= bStart || bEnd <= aStart) {
-        partial++;
+      const line = rawLine
+        .replace(/\s\s\s\s\s/gi, ' [ ] ')
+        .replace(/\s\s\s/gi, '[ ]')
+        .replace(/\[\s\]/gi, '[!]')
+        .replace(/\]\[/gi, '] [')
+        .replace(/\s\s/gi, ' ');
+
+      const items = line
+        .split(' ')
+        .filter((item) => item.length > 0)
+        .map((item) => {
+          return item.replace('[', '').replace(']', '');
+        });
+
+      for (let i = 0; i < items.length; i++) {
+        if (!stacks[i]) {
+          stacks[i] = [];
+        }
+
+        if (items[i] === '!') {
+          continue;
+        }
+
+        stacks[i].unshift(items[i]);
       }
     }
 
-    return `${partial} of ${lines.length}`;
+    for (const line of lines) {
+      if (!line.trim().startsWith('move')) {
+        continue;
+      }
+
+      const match = line.match(/move ([0-9]+)? from ([0-9]+)? to ([0-9]+)?/i);
+      const amount = Number(match[1]);
+      const sourceStackIndex = Number(match[2]) - 1;
+      const targetStackIndex = Number(match[3]) - 1;
+
+      if (amount <= 0 || amount > stacks[sourceStackIndex].length) {
+        console.log(`Invalid command! ${amount}: ${line}`);
+        continue;
+      }
+
+      const itemsToMove = stacks[sourceStackIndex].splice(stacks[sourceStackIndex].length - amount, amount);
+      for (const item of itemsToMove) {
+        stacks[targetStackIndex].push(item);
+      }
+    }
+
+    return this.topStacks(stacks);
+  },
+
+  topStacks(stacks) {
+    let result = '';
+    for (const stack of stacks) {
+      const item = stack[stack.length - 1];
+      result += item.toString();
+    }
+
+    return result;
   }
 };
